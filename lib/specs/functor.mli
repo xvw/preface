b611d80660962f53cb4ee5430f9149421e896b79
rigmm <grimfw@gmail.com>
2019-08-28 07:27:07 +0200

    Initialize Functor API (in Specs part)

---
+++ lib/specs/functor.mli
@@ -0,0 +1,51 @@
+(** A [Functor] for ['a t] is mappable from ['a] to ['b].
+    So for all ['a t], we can go to ['b t] using the [map] function.
+
+    To have a predictable behaviour, the instance of [Functor] must
+    obey some laws.
+
+    {1 Laws of [Functor]}
+    - [fmap id] must be equivalent to [id];
+    - [fmap (f <% g)] must be equivalent to [fmap f <% fmap g].
+*)
+
+(** {1 Requirement} *)
+
+module type REQUIREMENT = sig
+  type 'a t
+  (** The type holded by the [Functor]. *)
+
+  val map : ('a -> 'b) -> 'a t -> 'b t
+  (** Mapping over from ['a] to ['b] over ['a t] to ['b t]. *)
+end
+
+(** {1 API} *)
+
+(** The complete interface of a [Functor]. *)
+module type API = sig
+  include REQUIREMENT
+
+  val replace : 'a -> 'b t -> 'a t
+  (** Create a new ['a t], replacing all values in the ['b t]
+      by given a value of ['a].
+  *)
+
+  val void : 'a t -> unit t
+  (** Create a new [unit t], replacing all values in the ['a t] by [unit]. *)
+
+  module Infix : sig
+    val ( <$> ) : ('a -> 'b) -> 'a t -> 'b t
+    (** Infix version of {!val:map}. *)
+
+    val ( <&> ) : 'a t -> ('a -> 'b) -> 'b t
+    (** Flipped and infix version of {!val:map}. *)
+
+    val ( <$ ) : 'a -> 'b t -> 'a t
+    (** Infix version of {!val:replace}. *)
+
+    val ( $> ) : 'a t -> 'b -> 'b t
+    (** Flipped and infix version of {!val:replace}. *)
+  end
+
+  include module type of Infix
+end
