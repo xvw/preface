03569159bb6e709f2c18bd17354ec677c9e970c5
rigmm <grimfw@gmail.com>
2019-08-28 07:25:32 +0200

    Extension for Stdlib.Fun (to allowing composition)

---
+++ lib/preface/fun.ml
@@ -0,0 +1,12 @@
+let compose_left_to_right f g x = g (f x)
+
+let compose_right_to_left f g x = f (g x)
+
+module Infix = struct
+  let ( %> ) = compose_left_to_right
+
+  let ( <% ) = compose_right_to_left
+end
+
+include Infix
+include Stdlib.Fun
