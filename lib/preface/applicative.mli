50aba8c18dff46c6c83e019539685e62b1b85b11
pytre <grimfw@gmail.com>
2019-10-13 18:38:25 +0200

    Remove "for and ['a t]" since the type is member of the module

--- lib/preface/applicative.mli
+++ lib/preface/applicative.mli
@@ -192,15 +192,15 @@ module Make_via_map_and_product
     (Core_via_map_and_product : Preface_specs.Applicative
                                 .CORE_VIA_MAP_AND_PRODUCT) :
   Preface_specs.APPLICATIVE with type 'a t = 'a Core_via_map_and_product.t
-(** Incarnation of an [Applicative] for an ['a t] with standard
-    Requirements ([pure], [map] and [product]).
+(** Incarnation of an [Applicative] with standard requirements 
+    ([pure], [map] and [product]).
 *)
 
 module Make_via_apply
     (Core_via_apply : Preface_specs.Applicative.CORE_VIA_APPLY) :
   Preface_specs.APPLICATIVE with type 'a t = 'a Core_via_apply.t
-(** Incarnation of an [Applicative] for an ['a t] with standard
-    Requirements ([pure] and [apply]).
+(** Incarnation of an [Applicative] with standard requirements 
+    ([pure] and [apply]).
 *)
 
 module Make_via_monad (Monad : Preface_specs.MONAD) :
@@ -220,8 +220,7 @@ module Make
     (Infix : Preface_specs.Applicative.INFIX with type 'a t = 'a Core.t)
     (Syntax : Preface_specs.Applicative.SYNTAX with type 'a t = 'a Core.t) :
   Preface_specs.APPLICATIVE with type 'a t = 'a Core.t
-(** Incarnation of an [Applicative] for an ['a t] using each components of 
-    an [Applicative].
+(** Incarnation of an [Applicative] using each components of an [Applicative].
 *)
 
 module Make_core_via_map_and_product
@@ -233,26 +232,26 @@ module Make_core_via_map_and_product
 
 module Make_core_via_apply (Core : Preface_specs.Applicative.CORE_VIA_APPLY) :
   Preface_specs.Applicative.CORE with type 'a t = 'a Core.t
-(** Incarnation of an [Applicative.Core] for an ['a t] with standard
-    Requirements ([pure], [apply]).
+(** Incarnation of an [Applicative.Core] with standard requirements 
+    ([pure], [apply]).
 *)
 
 module Make_operation (Core : Preface_specs.Applicative.CORE) :
   Preface_specs.Applicative.OPERATION with type 'a t = 'a Core.t
-(** Incarnation of an [Applicative.Operation] for an ['a t] with standard
-    Requirements ([pure], [map], [apply] and [product]).
+(** Incarnation of an [Applicative.Operation] with standard requirements 
+    ([pure], [map], [apply] and [product]).
 *)
 
 module Make_syntax (Core : Preface_specs.Applicative.CORE) :
   Preface_specs.Applicative.SYNTAX with type 'a t = 'a Core.t
-(** Incarnation of an [Applicative.Syntax] for an ['a t] with standard
-    Requirements ([pure], [map], [apply] and [product]).
+(** Incarnation of an [Applicative.Syntax] with standard requirements 
+    ([pure], [map], [apply] and [product]).
 *)
 
 module Make_infix
     (Core : Preface_specs.Applicative.CORE)
     (Operation : Preface_specs.Applicative.OPERATION with type 'a t = 'a Core.t) :
   Preface_specs.Applicative.INFIX with type 'a t = 'a Core.t
-(** Incarnation of an [Applicative.Infix] for an ['a t] with standard
-    Requirements ([pure], [map], [apply] and [product]).
+(** Incarnation of an [Applicative.Infix] with standard requirements 
+    ([pure], [map], [apply] and [product]).
 *)
--- lib/preface/functor.mli
+++ lib/preface/functor.mli
@@ -106,9 +106,7 @@
 
 module Make_via_map (Core : Preface_specs.Functor.CORE) :
   Preface_specs.FUNCTOR with type 'a t = 'a Core.t
-(** Incarnation of a [Functor] for an ['a t] with standard
-    Requirements ([map]).
-*)
+(** Incarnation of a [Functor] with standard requirements ([map]).*)
 
 (** {2 Manual construction} 
     
@@ -122,20 +120,17 @@ module Make
     (Operation : Preface_specs.Functor.OPERATION with type 'a t = 'a Core.t)
     (Infix : Preface_specs.Functor.INFIX with type 'a t = 'a Core.t) :
   Preface_specs.FUNCTOR with type 'a t = 'a Core.t
-(** Incarnation of a [Functor] for an ['a t] using each components of 
+(** Incarnation of a [Functor] using each components of 
     a [Functor].
 *)
 
 module Make_operation (Core : Preface_specs.Functor.CORE) :
   Preface_specs.Functor.OPERATION with type 'a t = 'a Core.t
-(** Incarnation of a [Functor.Operation] for an ['a t] with standard
-    Requirements ([map]). 
+(** Incarnation of a [Functor.Operation] with standard Requirements ([map]). 
 *)
 
 module Make_infix
     (Core : Preface_specs.Functor.CORE)
     (Operation : Preface_specs.Functor.OPERATION with type 'a t = 'a Core.t) :
   Preface_specs.Functor.INFIX with type 'a t = 'a Core.t
-(** Incarnation of a [Functor.Infix] for an ['a t] with functional API of 
-    a [Functor]. 
-*)
+(** Incarnation of a [Functor.Infix] with functional API of a [Functor]. *)
