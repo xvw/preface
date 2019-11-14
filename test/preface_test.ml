c3620a4a9a2c2ea58422094e397c7757fc29e07f
d-plaindoux <d.plaindoux@free.fr>
2019-11-14 19:38:44 +0100

    feat(stream): First tests

---
+++ test/preface_test.ml
@@ -0,0 +1,9 @@
+let () =
+  let open Alcotest in
+  run
+    "Preface"
+    [ Fun_test.test_cases
+    ; Identity_functor_test.test_cases
+    ; Identity_applicative_test.test_cases
+    ; Identity_monad_test.test_cases
+    ; Stream_comonad_test.test_cases ]
