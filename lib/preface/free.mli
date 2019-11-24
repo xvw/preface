26faa28991a2a160b366287d61bdbda48d91189f
d-plaindoux <d.plaindoux@free.fr>
2019-11-24 19:07:43 +0100

    feature(free/freeer): Free Applicative type

--- lib/preface/free.mli
+++ lib/preface/free.mli
@@ -1,3 +1,5 @@
 (** Modules for building [Free] modules. *)
 
 module Make_free_functor (F : Specs.Functor.CORE) : Specs.Functor.CORE
+
+module Make_free_applicative (F : Specs.Functor.CORE) : Specs.Applicative.CORE_VIA_APPLY
