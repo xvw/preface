7e250717527654035bb75b2545a9faaab8ff2846
d-plaindoux <d.plaindoux@free.fr>
2019-11-24 19:02:09 +0100

    feature(free/freeer): Free Applicative first implementation with code repetition

--- lib/preface/free.ml
+++ lib/preface/free.ml
@@ -6,11 +6,18 @@ module Make_free_functor (F : Specs.Functor.CORE) : Specs.Functor.CORE = struct
     | FlatMap f' -> FlatMap (F.map (map f) f')
 end
 
-module Make_free_applicative (F : Specs.Applicative.CORE) : Specs.Functor.CORE =
-struct
+module Make_free_applicative (F : Specs.Functor.CORE) : Specs.Applicative.CORE_VIA_APPLY = struct
   include Specs.Free.CORE_with_type (F)
+
+  let pure a = Return a
 
   let rec map f = function
     | Return v -> Return (f v)
     | FlatMap f' -> FlatMap (F.map (map f) f')
+
+  let rec apply f a =
+    match f with
+    | Return f' -> map f' a
+    | FlatMap f' -> FlatMap (F.map (fun f -> apply f a) f')
+
 end
