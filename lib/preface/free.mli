bdd8a49b38dce9daac072583020722baede00048
d-plaindoux <d.plaindoux@free.fr>
2019-11-26 10:46:28 +0100

    feature(freeer): Provide Freeer generaliazed functor, applicative and monad

--- lib/preface/free.mli
+++ lib/preface/free.mli
@@ -1,10 +1,10 @@
 (** Modules for building [Free] modules. *)
 
 module Make_free_functor (F : Specs.Functor.CORE) :
-  Specs.Functor.CORE with type 'a t = 'a Specs.Free.CORE(F).t
+  Specs.FUNCTOR with type 'a t = 'a Specs.Free.CORE(F).t
 
 module Make_free_applicative (F : Specs.Functor.CORE) :
-  Specs.Applicative.CORE with type 'a t = 'a Specs.Free.CORE(F).t
+  Specs.APPLICATIVE with type 'a t = 'a Specs.Free.CORE(F).t
 
 module Make_free_monad (F : Specs.Functor.CORE) :
-  Specs.Monad.CORE with type 'a t = 'a Specs.Free.CORE(F).t
+  Specs.MONAD with type 'a t = 'a Specs.Free.CORE(F).t
--- lib/preface/freeer.ml
+++ lib/preface/freeer.ml
@@ -2,10 +2,43 @@ open Fun.Infix
 
 module Make_freeer_functor (F : sig
   type 'a t
-end) : Specs.Functor.CORE with type 'a t = 'a Specs.Freeer.CORE(F).t = struct
+end) : Specs.FUNCTOR with type 'a t = 'a Specs.Freeer.CORE(F).t =
+Functor.Make_via_map(struct
   include Specs.Freeer.CORE (F)
 
   let rec map f = function
     | Return x -> Return (f x)
     | FlatMap (i, c) -> FlatMap (i, c %> map f)
-end
+end)
+
+module Make_freeer_applicative (F : sig
+  type 'a t
+end) :
+  Specs.APPLICATIVE with type 'a t = 'a Specs.Freeer.CORE(F).t =
+Applicative.Make_via_apply (struct
+  include Specs.Freeer.CORE (F)
+  include Make_freeer_functor (F)
+
+  let pure a = Return a
+
+  let rec apply f a =
+    match f with
+    | Return f' -> map f' a
+    | FlatMap (i, c) -> FlatMap (i, c %> (fun f -> apply f a))
+end)
+
+module Make_freeer_monad (F : sig
+  type 'a t
+end) :
+  Specs.MONAD with type 'a t = 'a Specs.Freeer.CORE(F).t =
+Monad.Make_via_bind (struct
+  include Specs.Freeer.CORE (F)
+  include Make_freeer_functor (F)
+  include Make_freeer_applicative (F)
+
+  let return = pure
+
+  let rec bind f = function
+    | Return a -> f a
+    | FlatMap (i, c) -> FlatMap (i, c %> bind f)
+end)
