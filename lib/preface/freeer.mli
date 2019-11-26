776639e94deda40051e81e0c42ba3f928e44f2d2
d-plaindoux <d.plaindoux@free.fr>
2019-11-26 10:46:28 +0100

    feature(freeer): Provide Freeer generaliazed functor, applicative and monad

--- lib/preface/freeer.mli
+++ lib/preface/freeer.mli
@@ -2,4 +2,13 @@
 
 module Make_freeer_functor (F : sig
   type 'a t
-end) : Specs.Functor.CORE
+end) : Specs.FUNCTOR with type 'a t = 'a Specs.Freeer.CORE(F).t
+
+module Make_freeer_applicative (F : sig
+  type 'a t
+end) : Specs.APPLICATIVE with type 'a t = 'a Specs.Freeer.CORE(F).t
+
+module Make_freeer_monad (F : sig
+  type 'a t
+end) : Specs.MONAD with type 'a t = 'a Specs.Freeer.CORE(F).t
+
