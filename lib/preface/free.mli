aae05eeee16be2d9f96e09ad1c7e0a2dd3c8945e
d-plaindoux <d.plaindoux@free.fr>
2019-11-26 09:38:17 +0100

    feature(free): Free monad and generalized types for Free applicative

--- lib/preface/free.mli
+++ lib/preface/free.mli
@@ -1,6 +1,10 @@
 (** Modules for building [Free] modules. *)
 
-module Make_free_functor (F : Specs.Functor.CORE) : Specs.Functor.CORE
+module Make_free_functor (F : Specs.Functor.CORE) :
+  Specs.Functor.CORE with type 'a t = 'a Specs.Free.CORE(F).t
 
 module Make_free_applicative (F : Specs.Functor.CORE) :
-  Specs.Applicative.CORE_VIA_APPLY
+  Specs.Applicative.CORE with type 'a t = 'a Specs.Free.CORE(F).t
+
+module Make_free_monad (F : Specs.Functor.CORE) :
+  Specs.Monad.CORE with type 'a t = 'a Specs.Free.CORE(F).t
