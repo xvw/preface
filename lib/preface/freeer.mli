ea8311014790bc2f40b76bd818d8cb2271c713c9
d-plaindoux <d.plaindoux@free.fr>
2019-11-26 08:24:56 +0100

    feature(free/freeer): Code revision and simplification removing the intermediate Core type.

--- lib/preface/freeer.mli
+++ lib/preface/freeer.mli
@@ -1,3 +1,5 @@
 (** Modules for building [Freeer] modules. *)
 
-module Make_freeer_functor (F : sig type 'a t end) : Specs.Functor.CORE
+module Make_freeer_functor (F : sig
+  type 'a t
+end) : Specs.Functor.CORE
