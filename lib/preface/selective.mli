67cf238f8e513c7b6a7ea7dad5621f76afbe53fd
pytre <grimfw@gmail.com>
2019-11-28 08:04:20 +0100

    Fix selective reference to specs

--- lib/preface/selective.mli
+++ lib/preface/selective.mli
@@ -3,53 +3,53 @@
 (** {1 Construction of a [Selective] module} *)
 
 module Make_via_applicative
-    (Applicative : Specs.APPLICATIVE)
-    (Select : Specs.Selective.CORE_VIA_SELECT with type 'a t = 'a Applicative.t) :
-  Specs.SELECTIVE with type 'a t = 'a Select.t
+    (Applicative : Preface_specs.APPLICATIVE)
+    (Select : Preface_specs.Selective.CORE_VIA_SELECT with type 'a t = 'a Applicative.t) :
+  Preface_specs.SELECTIVE with type 'a t = 'a Select.t
 
 module Make_via_functor
-    (Functor : Specs.FUNCTOR)
-    (Select : Specs.Selective.CORE_VIA_SELECT with type 'a t = 'a Functor.t) :
-  Specs.SELECTIVE with type 'a t = 'a Select.t
+    (Functor : Preface_specs.FUNCTOR)
+    (Select : Preface_specs.Selective.CORE_VIA_SELECT with type 'a t = 'a Functor.t) :
+  Preface_specs.SELECTIVE with type 'a t = 'a Select.t
 
 module Make
-    (Core : Specs.Selective.CORE)
-    (Operation : Specs.Selective.OPERATION
+    (Core : Preface_specs.Selective.CORE)
+    (Operation : Preface_specs.Selective.OPERATION
                    with type 'a t = 'a Core.t
                     and module Either = Core.Either)
-    (Infix : Specs.Selective.INFIX
+    (Infix : Preface_specs.Selective.INFIX
                with type 'a t = 'a Core.t
                 and module Either = Core.Either)
-    (Syntax : Specs.Selective.SYNTAX with type 'a t = 'a Core.t) :
-  Specs.SELECTIVE with type 'a t = 'a Core.t
+    (Syntax : Preface_specs.Selective.SYNTAX with type 'a t = 'a Core.t) :
+  Preface_specs.SELECTIVE with type 'a t = 'a Core.t
 
 (** {1 Internal construction of a [Selective] module} *)
 
 module Make_core_via_functor
-    (Functor : Specs.FUNCTOR)
-    (Select : Specs.Selective.CORE_VIA_SELECT with type 'a t = 'a Functor.t) :
-  Specs.Selective.CORE
+    (Functor : Preface_specs.FUNCTOR)
+    (Select : Preface_specs.Selective.CORE_VIA_SELECT with type 'a t = 'a Functor.t) :
+  Preface_specs.Selective.CORE
     with type 'a t = 'a Functor.t
      and module Either = Select.Either
 
 module Make_core_via_applicative
-    (Applicative : Specs.APPLICATIVE)
-    (Select : Specs.Selective.CORE_VIA_SELECT with type 'a t = 'a Applicative.t) :
-  Specs.Selective.CORE
+    (Applicative : Preface_specs.APPLICATIVE)
+    (Select : Preface_specs.Selective.CORE_VIA_SELECT with type 'a t = 'a Applicative.t) :
+  Preface_specs.Selective.CORE
     with type 'a t = 'a Applicative.t
      and module Either = Select.Either
 
-module Make_operation (Core : Specs.Selective.CORE) :
-  Specs.Selective.OPERATION
+module Make_operation (Core : Preface_specs.Selective.CORE) :
+  Preface_specs.Selective.OPERATION
     with type 'a t = 'a Core.t
      and module Either = Core.Either
 
 module Make_infix
-    (Core : Specs.Selective.CORE)
-    (Operation : Specs.Selective.OPERATION with type 'a t = 'a Core.t) :
-  Specs.Selective.INFIX
+    (Core : Preface_specs.Selective.CORE)
+    (Operation : Preface_specs.Selective.OPERATION with type 'a t = 'a Core.t) :
+  Preface_specs.Selective.INFIX
     with type 'a t = 'a Core.t
      and module Either = Core.Either
 
-module Make_syntax (Core : Specs.Selective.CORE) :
-  Specs.Selective.SYNTAX with type 'a t = 'a Core.t
+module Make_syntax (Core : Preface_specs.Selective.CORE) :
+  Preface_specs.Selective.SYNTAX with type 'a t = 'a Core.t
