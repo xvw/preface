fb2b4e40f4eaea344dcc36129219ba79465274c7
pytre <grimfw@gmail.com>
2020-04-08 01:53:31 +0200

    Initialize preface_laws_test module and add identity tests

---
+++ test/preface_laws_test/identity_test.ml
@@ -0,0 +1,30 @@
+open Preface_test
+include Preface_stdlib.Identity
+
+module Requirement = struct
+  type nonrec 'a t = 'a t
+
+  let suite_name = "Identity"
+
+  let arbitrary = Qcheck_helpers.Arbitrary.identity
+end
+
+module Identity_functor =
+  Functor_laws.Make (Functor) (Requirement) (Qcheck_helpers.Sample.Int)
+    (Qcheck_helpers.Sample.String)
+    (Qcheck_helpers.Sample.String)
+module Identity_applicative =
+  Applicative_laws.Make (Applicative) (Requirement) (Qcheck_helpers.Sample.Int)
+    (Qcheck_helpers.Sample.String)
+    (Qcheck_helpers.Sample.Int)
+    (Qcheck_helpers.Sample.String)
+    (Qcheck_helpers.Sample.Int)
+module Identity_monad =
+  Monad_laws.Make (Monad) (Requirement) (Qcheck_helpers.Sample.Int)
+    (Qcheck_helpers.Sample.String)
+    (Qcheck_helpers.Sample.Int)
+    (Qcheck_helpers.Sample.String)
+
+let cases =
+  [ Identity_functor.cases; Identity_applicative.cases; Identity_monad.cases ]
+;;
