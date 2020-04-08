6a7d34b1fdf9912da2317272e7a5681c925e124b
xvw <xaviervdw@gmail.com>
2020-04-08 15:52:15 +0200

    Improve Stream API

--- lib/preface_stdlib/stream.ml
+++ lib/preface_stdlib/stream.ml
@@ -2,6 +2,47 @@ type 'a t = C of 'a * 'a t Lazy.t
 
 let stream a l = C (a, l)
 
+let rec pure x = C (x, lazy (pure x))
+
+let hd = function C (x, _) -> x
+
+let tl = function C (_, xs) -> Lazy.force xs
+
+let rec map f = function C (x, xs) -> C (f x, lazy (map f @@ Lazy.force xs))
+
+module Functor = Preface_make.Functor.Via_map (struct
+  type nonrec 'a t = 'a t
+
+  let map = map
+end)
+
+module Applicative = Preface_make.Applicative.Via_apply (struct
+  type nonrec 'a t = 'a t
+
+  let pure = pure
+
+  let rec apply stream1 stream2 =
+    let f g x = g x in
+    match (stream1, stream2) with
+    | (C (x, xs), C (y, ys)) ->
+      C (f x y, lazy (apply (Lazy.force xs) (Lazy.force ys)))
+  ;;
+end)
+
+module Monad = Preface_make.Monad.Via_map_and_join (struct
+  type nonrec 'a t = 'a t
+
+  let return = pure
+
+  let map = map
+
+  let rec join = function
+    | C (first, second) ->
+      let tail = lazy (map tl @@ Lazy.force second) in
+      C (hd first, lazy (join @@ Lazy.force tail))
+  ;;
+end)
+
 module Comonad = Preface_make.Comonad.Via_map_and_duplicate (struct
   type nonrec 'a t = 'a t
 
--- lib/preface_stdlib/stream.mli
+++ lib/preface_stdlib/stream.mli
@@ -1,5 +1,40 @@
+(** Exposes [Stream.t], a kind of inifinite lists
+
+    {1 Capabilities}
+
+    - {!val:Functor}
+    - {!val:Applicative}
+    - {!val:Monad}    
+    - {!val:Comonad}
+*)
+
+(** {1 Type} *)
 type 'a t
 
-val stream : 'a -> 'a t Lazy.t -> 'a t
+(** {1 Implementation} *)
+
+module Functor : Preface_specs.FUNCTOR with type 'a t = 'a t
+(** {2 Functor API} *)
+
+module Applicative : Preface_specs.APPLICATIVE with type 'a t = 'a t
+(** {2 Applicative API} *)
+
+module Monad : Preface_specs.MONAD with type 'a t = 'a t
+(** {2 Monad API} *)
 
 module Comonad : Preface_specs.COMONAD with type 'a t = 'a t
+(** {2 Comonad API} *)
+
+(** {1 Helpers} *)
+
+val pure : 'a -> 'a t
+(** Create a value from ['a] to ['a t]. *)
+                                                
+val stream : 'a -> 'a t Lazy.t -> 'a t
+(** Build a stream manually. *)
+
+val hd : 'a t -> 'a
+(** Get the head of the stream. *)
+
+val tl : 'a t -> 'a t
+(** Get the tail of the stream. *)
