06a52effbb2ab7ad3f0b3f14d9e1cc4801d6b399
pytre <grimfw@gmail.com>
2020-04-19 18:14:00 +0200

    Add test for selective identity

--- test/preface_stdlib_test/identity_test.ml
+++ test/preface_stdlib_test/identity_test.ml
@@ -13,10 +13,164 @@ module Monad_test = Support.Monad (struct
   include Monad
 end)
 
+open Preface.Identity
+open Selective
+module Either = Preface_core.Either
+
+let identity a = Alcotest.testable (pp (Alcotest.pp a)) (eq ( = ))
+
+let should_select_left () =
+  let open Either in
+  let expected = pure 42
+  and computed = select (pure @@ left 40) (pure @@ ( + ) 2) in
+  Alcotest.(check (identity int)) "should_select_left" expected computed
+;;
+
+let should_select_right () =
+  let open Either in
+  let expected = pure 42
+  and computed = select (pure @@ right 42) (pure @@ ( + ) 2) in
+  Alcotest.(check (identity int)) "should_select_right" expected computed
+;;
+
+let should_branch_left () =
+  let open Either in
+  let expected = pure 42
+  and computed = branch (pure @@ left 40) (pure @@ ( + ) 2) (pure @@ ( + ) 4) in
+  Alcotest.(check (identity int)) "should_branch_left" expected computed
+;;
+
+let should_branch_right () =
+  let open Either in
+  let expected = pure 42
+  and computed =
+    branch (pure @@ right 40) (pure @@ ( + ) 4) (pure @@ ( + ) 2)
+  in
+  Alcotest.(check (identity int)) "should_branch_right" expected computed
+;;
+
+let should_if_then_left () =
+  let expected = pure 42
+  and computed = if_ (pure true) (pure 42) (pure 40) in
+  Alcotest.(check (identity int)) "should_if_then_left" expected computed
+;;
+
+let should_if_else_right () =
+  let expected = pure 42
+  and computed = if_ (pure false) (pure 40) (pure 42) in
+  Alcotest.(check (identity int)) "should_if_else_right" expected computed
+;;
+
+let should_infix_select_left () =
+  let open Either in
+  let expected = pure 42
+  and computed = pure @@ left 40 <?* pure @@ ( + ) 2 in
+  Alcotest.(check (identity int)) "should_infix_select_left" expected computed
+;;
+
+let should_infix_select_right () =
+  let open Either in
+  let expected = pure 42
+  and computed = pure @@ right 42 <?* pure @@ ( + ) 2 in
+  Alcotest.(check (identity int)) "should_infix_select_right" expected computed
+;;
+
+let should_swapped_infix_select_left () =
+  let open Either in
+  let expected = pure 42
+  and computed = (pure @@ ( + ) 2) *?> (pure @@ left 40) in
+  Alcotest.(check (identity int))
+    "should_swapped_infix_select_left" expected computed
+;;
+
+let should_swapped_infix_select_right () =
+  let open Either in
+  let expected = pure 42
+  and computed = (pure @@ ( + ) 2) *?> (pure @@ right 42) in
+  Alcotest.(check (identity int))
+    "should_swapped_infix_select_right" expected computed
+;;
+
+let should_or_true_false () =
+  let expected = pure true
+  and computed = pure true <||> pure false in
+  Alcotest.(check (identity bool)) "should_or_true_false" expected computed
+;;
+
+let should_or_false_false () =
+  let expected = pure false
+  and computed = pure false <||> pure false in
+  Alcotest.(check (identity bool)) "should_or_false_false" expected computed
+;;
+
+let should_or_true_true () =
+  let expected = pure true
+  and computed = pure true <||> pure true in
+  Alcotest.(check (identity bool)) "should_or_true_true" expected computed
+;;
+
+let should_or_false_true () =
+  let expected = pure true
+  and computed = pure false <||> pure true in
+  Alcotest.(check (identity bool)) "should_or_false_true" expected computed
+;;
+
+let should_and_true_false () =
+  let expected = pure false
+  and computed = pure true <&&> pure false in
+  Alcotest.(check (identity bool)) "should_and_true_false" expected computed
+;;
+
+let should_and_false_false () =
+  let expected = pure false
+  and computed = pure false <&&> pure false in
+  Alcotest.(check (identity bool)) "should_and_false_false" expected computed
+;;
+
+let should_and_true_true () =
+  let expected = pure true
+  and computed = pure true <&&> pure true in
+  Alcotest.(check (identity bool)) "should_and_true_true" expected computed
+;;
+
+let should_and_false_true () =
+  let expected = pure false
+  and computed = pure false <&&> pure true in
+  Alcotest.(check (identity bool)) "should_and_false_true" expected computed
+;;
+
+let selective_cases =
+  let open Alcotest in
+  ( "Selective Applicative"
+  , [
+      test_case "Select left" `Quick should_select_left
+    ; test_case "Select right" `Quick should_select_right
+    ; test_case "Branch left" `Quick should_branch_left
+    ; test_case "Branch right" `Quick should_branch_right
+    ; test_case "If then left" `Quick should_if_then_left
+    ; test_case "If else right" `Quick should_if_else_right
+    ; test_case "Infix select left" `Quick should_infix_select_left
+    ; test_case "Infix select right" `Quick should_infix_select_right
+    ; test_case "Swapped infix select left" `Quick
+        should_swapped_infix_select_left
+    ; test_case "Swapped infix select right" `Quick
+        should_swapped_infix_select_right
+    ; test_case "Or true false" `Quick should_or_true_false
+    ; test_case "Or true true" `Quick should_or_true_true
+    ; test_case "Or false false" `Quick should_or_false_false
+    ; test_case "Or false true" `Quick should_or_false_true
+    ; test_case "And true false" `Quick should_and_true_false
+    ; test_case "And true true" `Quick should_and_true_true
+    ; test_case "And false false" `Quick should_and_false_false
+    ; test_case "And false true" `Quick should_and_false_true
+    ] )
+;;
+
 let test_cases =
   [
     ("Identity Functor", Functor_test.cases)
   ; ("Identity Applicative", Applicative_test.cases)
   ; ("Identity Monad", Monad_test.cases)
+  ; selective_cases
   ]
 ;;
