81c5b991590c98dd681fecf8fbd290d27b873dd1
xvw <xaviervdw@gmail.com>
2020-05-05 12:15:57 +0200

    Add Traversable implementation over Nonempty list

--- lib/preface_stdlib/nonempty_list.ml
+++ lib/preface_stdlib/nonempty_list.ml
@@ -1,4 +1,10 @@
-include Preface_core.Nonempty_list
+type 'a t = 'a Preface_core.Nonempty_list.t =
+  | Last of 'a
+  | ( :: ) of ('a * 'a t)
+
+include (
+  Preface_core.Nonempty_list :
+    module type of Preface_core.Nonempty_list with type 'a t := 'a t )
 
 let pure x = create x
 
@@ -8,23 +14,78 @@ module Functor = Preface_make.Functor.Via_map (struct
   let map = map
 end)
 
-module Applicative = Preface_make.Applicative.Via_apply (struct
-  type nonrec 'a t = 'a t
+module Applicative = struct
+  module A = Preface_make.Applicative.Via_apply (struct
+    type nonrec 'a t = 'a t
 
-  let pure = pure
+    let pure = pure
 
-  let apply fs xs = flatten @@ map (fun f -> map (fun x -> f x) xs) fs
-end)
+    let apply fs xs = flatten @@ map (fun f -> map (fun x -> f x) xs) fs
+  end)
 
-module Monad = Preface_make.Monad.Via_map_and_join (struct
-  type nonrec 'a t = 'a t
+  module Traversable (A : Preface_specs.APPLICATIVE) :
+    Preface_specs.TRAVERSABLE with type 'a t = 'a A.t and type 'a iter = 'a t =
+  struct
+    module T = struct
+      type 'a t = 'a A.t
 
-  let return = pure
+      type 'a iter = 'a Preface_core.Nonempty_list.t
 
-  let map = map
+      let traverse =
+        let open A.Infix in
+        let rec traverse_aux f = function
+          | Last x -> (fun x -> Last x) <$> f x
+          | x :: xs -> A.lift2 cons (f x) (traverse_aux f xs)
+        in
+        traverse_aux
+      ;;
+    end
 
-  let join = flatten
-end)
+    include Preface_make.Traversable.Over_applicative (A) (T)
+  end
+
+  include A
+end
+
+module Monad = struct
+  module M = Preface_make.Monad.Via_map_and_join (struct
+    type nonrec 'a t = 'a t
+
+    let return = pure
+
+    let map = map
+
+    let join = flatten
+  end)
+
+  module Traversable (M : Preface_specs.MONAD) :
+    Preface_specs.TRAVERSABLE with type 'a t = 'a M.t and type 'a iter = 'a t =
+  struct
+    module T = struct
+      type 'a t = 'a M.t
+
+      type 'a iter = 'a Preface_core.Nonempty_list.t
+
+      let traverse =
+        let open M.Syntax in
+        let rec traverse_aux f = function
+          | Last x ->
+            let+ elt = f x in
+            Last elt
+          | x :: xs ->
+            let* h = f x in
+            let* t = traverse_aux f xs in
+            M.return (cons h t)
+        in
+        traverse_aux
+      ;;
+    end
+
+    include Preface_make.Traversable.Over_monad (M) (T)
+  end
+
+  include M
+end
 
 module Selective =
   Preface_make.Selective.Over_applicative
--- lib/preface_stdlib/nonempty_list.mli
+++ lib/preface_stdlib/nonempty_list.mli
@@ -7,15 +7,27 @@
     - {!val:Selective}
     - {!val:Monad} *)
 
-include module type of Preface_core.Nonempty_list
+(** {1 Type} *)
+
+type 'a t = 'a Preface_core.Nonempty_list.t =
+  | Last of 'a
+  | ( :: ) of ('a * 'a t)
 
 (** {1 Implementation} *)
 
 module Functor : Preface_specs.FUNCTOR with type 'a t = 'a t
 (** {2 Functor API} *)
 
-module Applicative : Preface_specs.APPLICATIVE with type 'a t = 'a t
-(** {2 Applicative API} *)
+(** {2 Applicative API}
+
+    Applicative and Traversable for [Nonempty_list.t]. *)
+module Applicative : sig
+  (** {2 Traversable using Applicative form} *)
+  module Traversable (A : Preface_specs.APPLICATIVE) :
+    Preface_specs.TRAVERSABLE with type 'a t = 'a A.t and type 'a iter = 'a t
+
+  include Preface_specs.APPLICATIVE with type 'a t = 'a t
+end
 
 (** {2 Selective API} *)
 module Selective :
@@ -23,10 +35,20 @@ module Selective :
     with type 'a t = 'a t
      and type ('a, 'b) either = ('a, 'b) Preface_core.Either.t
 
-module Monad : Preface_specs.MONAD with type 'a t = 'a t
-(** {2 Monad API} *)
+(** {2 Monad API}
+
+    Monad and Traversable for [Nonempty_list.t].*)
+module Monad : sig
+  (** {2 Traversable using Monadic form} *)
+  module Traversable (M : Preface_specs.MONAD) :
+    Preface_specs.TRAVERSABLE with type 'a t = 'a M.t and type 'a iter = 'a t
+
+  include Preface_specs.MONAD with type 'a t = 'a t
+end
 
 (** {1 Helpers} *)
 
 val pure : 'a -> 'a t
 (** Create a value from ['a] to ['a t]. *)
+
+include module type of Preface_core.Nonempty_list with type 'a t := 'a t
